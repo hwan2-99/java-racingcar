ec58f09fc4af278b7b7f6ab73a99c577a830e0e6
hwan2-99 <thh1233123@gmail.com>
2024-09-23 23:08:11 +0900

    test: (1,2)값이 주어졌을 때 ()를 제거하고 1,2가 반환되는지 확인하는 테스트 작성

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -21,6 +21,14 @@ public class StringTest {
     }
 
     @Test
+    @DisplayName("(1,2)값이 주어졌을 때 ()를 제거하고 1,2가 반환되는지 확인")
+    void subStringTest(){
+        String s = "(1,2)";
+        String[] result = s.substring(1,4).split(",");
+        assertThat(result).containsExactly("1", "2");
+    }
+
+    @Test
     @DisplayName("특정 위치의 문자를 성공적으로 반환한다.")
     void indexTest() {
         String value = "abc";
